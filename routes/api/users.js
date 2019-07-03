b650bb0058a748347f17cecf93aaa27c05dbc116
Leah-Grace <leahgdg@hotmail.com>
2019-07-03 10:31:28 -0400

    refined routes, developing POST

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/user");
+const gravtar = require("gravatar");
 
 //@route GET api/users/test
 //@desc test users route
 //@access Public
 router.get("/test", (req, res) => res.json({ msg: "users works" }));
+
+//@route GET api/users/register
+//@desc register users route
+//@access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "Email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", //size
+        r: "pg", //rating
+        d: "mm" //default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+    }
+  });
+});
 
 module.exports = router;
