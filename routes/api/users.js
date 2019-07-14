db4b2c37c1e2eb1bee898ea9534327c6d8d83969
Leah-Grace <leahgdg@hotmail.com>
2019-07-14 16:15:19 -0400

    including bcrypt in user Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/user');
-const gravtar = require('gravatar');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //@route    POST api/users
 //@desc     Register user
@@ -19,16 +21,54 @@ router.post(
       'Please enter a passwordwith 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     // console.log(req.body); //objec of data, required body-parser
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+    const { name, email, password } = req.body;
+
+    try {
+      //see if user exists already >> send error
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'user already exists' }] });
+      }
+
+      //Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+      //encrypt with bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      user.passowrd = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send(`Server error`);
+    }
   }
 );
 
+// ORIGINAL REGISTATION ROUTE
+/*
 //@route GET api/users/register
 //@desc register users route
 //@access Public
@@ -40,7 +80,7 @@ router.post('/register', (req, res) => {
       const avatar = gravatar.url(req.body.email, {
         s: '200', //size
         r: 'pg', //rating
-        d: 'mm' //default
+        d: 'mm' //default instead of 404 (file not found errorr)
       });
       const newUser = new User({
         name: req.body.name,
@@ -51,5 +91,6 @@ router.post('/register', (req, res) => {
     }
   });
 });
+*/
 
 module.exports = router;
